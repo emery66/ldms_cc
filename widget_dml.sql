afb60c922a4f912c337d9168cf649d303a889fd5
emery66 <70519640+emery66@users.noreply.github.com>
2020-08-31 15:58:09 +0100

    Update widget_dml.sql

--- widget_dml.sql
+++ widget_dml.sql
@@ -28,23 +28,23 @@ VALUES(90010,’Hall’,’Mildred’,’12-OCT-96’,trunc(sysdate),'Ali');
 
 COMMIT;
 
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90001,'CEO',' ','01-JAN-95',100000,1,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90002,'Manager',90001,'23-SEP-03',52500,4,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90003,'Salesperson',90002,'11-FEB-17',35000,4,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90004,'Salesperson',90003,'20-MAY-15',32000,4,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90005,'Engineer',90006,'17-APR-18',40000,2,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90006,'Manager',90001,'21-MAR-15',45000,2,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90007,'Engineer',90006,'01-JAN-13',47000,2,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90008,'Engineer',90009,'27-JUL-14',45000,3,trunc(sysdate),'Ali');
-INSERT INTO EMPLOYEE_HISTORY(emph_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
+INSERT INTO EMPLOYEE_HISTORY(emph_emp_id,emph_job_title,emph_manager_id,emph_start_date,emph_salary,emph_dept_id,emph_date_entered,emph_entered_by)
 VALUES(90009,'Manager',90001,'15-MAY-18',50000,3,trunc(sysdate),'Ali');
 
 COMMIT;
