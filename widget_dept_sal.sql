f875fa35ab6eb10742d237e6877109ad93f63603
emery66 <70519640+emery66@users.noreply.github.com>
2020-08-31 18:56:07 +0100

    Update widget_dept_sal.sql

--- widget_dept_sal.sql
+++ widget_dept_sal.sql
@@ -3,13 +3,14 @@ dept_sal.sql
 REM AS 31/08/20 sql to show sum employee sal for a dept 
 
 Set stuff on and off
-Define &1 is dept no
+Define req_dept is '&1'
 
-Col a heading ‘Department’ format a50
+col dept
+Col  heading ‘Department’ format a50
 Col b heading ‘Sum Employee Salary’ format 999,999,999
 Col c heading noprint dep_id new_value
 
-Ttitle ‘Total Employees Salary for ‘&depname’  Dept’  
+Ttitle ‘Total Employees Salary for ‘&req_dept’ Department’  
 Select d.dept_name  a,
             Sum(e.emp_salary) b 
 From departments d,
