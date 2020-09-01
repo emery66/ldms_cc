b446cc7d8280e92c43e4e238321c01952575a11e
emery66 <70519640+emery66@users.noreply.github.com>
2020-09-01 09:03:40 +0100

    Update widget_dept_emps.sql

--- widget_dept_emps.sql
+++ widget_dept_emps.sql
@@ -1,7 +1,13 @@
-set verify off
-set termout off
+-- AS 31/08/20 first write
+
+-- AS add others to suit.
+set lines 120
+set pages 9999
+set newpage 0
 
 def req_dept = '&1'
+
+-- AS add stuff here to use a sequence to make filename unique
 
 col deptid heading 'Dept Id' format 99999
 col deptname heading 'Dept Name' format a50
@@ -11,6 +17,10 @@ Col job heading ‘Job Title’ format a50
 Col man heading ‘Managed By’ format a50 
 Col sal heading ‘Salary’ format 999,999 
 Col sdte heading ‘Start Date’ format date
+
+break on report
+
+spool dept_emps.lst
 
 Ttitle ‘Employees for '&req_dept' Department'
 
@@ -28,3 +38,6 @@ and upper(d.dept_name) = upper('&req_dept')
 and e.emp_manager_id = m.emp_id
 order by e.emp_id;
 
+spool off
+
+exit;
